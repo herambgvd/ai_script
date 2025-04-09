b448acc0a159a5461bffd380bb4695e64f90f5fd
herambmishra <herambmishra@geniusvision.in>
2025-04-09 11:24:05 +0530

    FD Update

--- prod_fd_yolo.py
+++ prod_fd_yolo.py
@@ -148,13 +148,13 @@ def process_stream(cap, out, ai_model, cam_id, cam_name, roi, producer):
             out.write(frame)
 
         # ✅ Show the inference window
-        cv2.imshow("Face Detection", frame)
-        if cv2.waitKey(1) & 0xFF == 27:  # Press 'Esc' to exit
-            break
+        # cv2.imshow("Face Detection", frame)
+        # if cv2.waitKey(1) & 0xFF == 27:  # Press 'Esc' to exit
+        #     break
 
     cap.release()
     out.release()
-    cv2.destroyAllWindows()
+    # cv2.destroyAllWindows()
 
 
 # ✅ Main Process
